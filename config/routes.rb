80f2bfec9855f0ae6f48219a7bfdbc49e870bdd8
senenalonso <senen.alonso@bbva.com>
2017-02-21 22:31:43 +0100

    Init devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-	get 	'/',	to: 'site#index',	as: :home 
+  devise_for :users, 	path: '',
+  										path_names: {sign_in: 'login', sign_up: 'signup'},
+  										controllers: {sessions: 'users/sessions'}
+
+  get 	'/users/:id', to: 'users#show'
+	get 	'/',					to: 'site#index',	as: :home 
 end
